466d521fcef7668d54e1dc78a107f459b9127272
nabil280905 <Nabilseptisyabana3@gmail.com>
2025-07-09 22:56:46 +0700

    update

--- api/index.js
+++ api/index.js
@@ -2,7 +2,9 @@ import jsonServer from 'json-server'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, '..', 'db.json'))
 const middlewares = jsonServer.defaults()
